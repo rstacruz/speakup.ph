28a1e02979a65fe5e075d1be65698ea2eae98bc2
Rico Sta. Cruz <rico@ambiescent.com>
2012-10-04 05:36:30 +0800

    Updates to senator lists.

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 (function() {
   var template = _.template($("#senator-template").html());
 
+  // Partial
+  function formatValue(val, type, section) {
+    var m;
+    if (section === 'Info') {
+      // External info
+      return "<a href='"+val+"' class='pre site'>"+type+"</a>";
+    } else if (m = val.match(/facebook\.com\/(?:pages\/)?(.*?)(?:\/[0-9]+)?$/)) {
+      // Facebook
+      return "<a href='"+val+"' class='pre facebook'>"+m[1]+"</a>";
+    } else if (m = val.match(/twitter\.com\/(.*)$/)) {
+      // Twitter
+      return "<a href='"+val+"' class='pre twitter'>@"+m[1]+"</a>";
+    } else if (m = val.match(/@[a-z\.]+$/)) {
+      // Email
+      return "<a href='mailto:"+val+"' class='pre email'>"+val+"</a>";
+    } else if (m = val.match(/^https?:\/\/([^\/]+)/)) {
+      // Website
+      return "<a href='"+val+"' class='pre site'>"+m[1]+"</a>";
+    } else {
+      return "<span class='other'>"+val+"</span>";
+    }
+  }
+
   Addresses = {
     load: function(data) {
       var $parent = $("<div>");
 
       $.each(data.contactInfo, function(name, data) {
-        var $el = template({ data: data, name: name });
+        var $el = template({ data: data, name: name, formatValue: formatValue });
         $parent.append($el);
       });
 
