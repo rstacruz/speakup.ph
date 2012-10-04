86aba182f63b7e19bb44f37aacd9a4a931f66774
Rico Sta. Cruz <rico@ambiescent.com>
2012-10-04 08:43:31 +0800

    Anchorjump.

--- script.js
+++ script.js
@@ -1,4 +1,68 @@
+/*! ---------------------------------------
+ *  Anchorjump
+ * ---------------------------------------- */
+
+(function($) {
+  var defaults = {
+    speed: 500,
+    offset: 0,
+    for: null,
+    parent: null
+  };
+
+  $.fn.anchorjump = function(options) {
+    options = $.extend({}, defaults, options);
+
+    if (options['for']) {
+      this.on('click', options['for'], onClick);
+    } else {
+      this.on('click', onClick);
+    }
+
+    function onClick(e) {
+      var $a = $(e.target).closest('a');
+      if (e.ctrlKey || e.metaKey || e.altKey || $a.attr('target')) return;
+
+      e.preventDefault();
+      var href = $a.attr('href');
+
+      $.anchorjump(href, options);
+    };
+  };
+
+  // Jump to a given area.
+  $.anchorjump = function(href, options) {
+    options = $.extend({}, defaults, options);
+
+    var top = 0;
+
+    if (href != '#') {
+      var $area = $(href);
+      // Find the parent
+      if (options.parent) {
+        var $parent = $area.closest(options.parent);
+        if ($parent.length) { $area = $parent; }
+      }
+      if (!$area.length) { return; }
+      top = $area.offset().top + options.offset;
+    }
+
+    $('html, body').animate({ scrollTop: top }, options.speed);
+    $('body').trigger('anchor', href);
+
+    // Add the location hash via pushState.
+    if (window.history.pushState) {
+      window.history.pushState({ href: href }, "", href);
+    }
+  };
+})(jQuery);
+
 (function() {
+  // Anchorjump
+  $(function() {
+    $("body").anchorjump({ for: 'a[href^="#"]', speed: 1000  });
+  });
+
   var template = _.template($("#senator-template").html());
 
   // Partial
